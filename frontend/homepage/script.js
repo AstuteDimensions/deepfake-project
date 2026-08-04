0f34a7c6685e0ef48c39da6dd341caa4185d8fc6
Anshal Shaju <anshalshaju0101@gmail.com>
2026-08-04 15:03:05 +0530

    Created login page

---
+++ frontend/homepage/script.js
@@ -0,0 +1,51 @@
+const password =
+document.getElementById("password");
+
+
+const toggle =
+document.getElementById("togglePassword");
+
+
+toggle.addEventListener(
+"click",
+()=>{
+
+
+    if(password.type==="password"){
+
+        password.type="text";
+
+        toggle.classList.remove("fa-eye");
+
+        toggle.classList.add("fa-eye-slash");
+
+    }
+
+    else{
+
+        password.type="password";
+
+        toggle.classList.remove("fa-eye-slash");
+
+        toggle.classList.add("fa-eye");
+
+    }
+
+
+});
+
+
+
+document
+.getElementById("loginForm")
+.addEventListener(
+"submit",
+(e)=>{
+
+    e.preventDefault();
+
+    alert(
+    "Login system will be connected to backend authentication."
+    );
+
+});
